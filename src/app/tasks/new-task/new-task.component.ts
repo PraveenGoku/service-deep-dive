eb8135e541cf908a8486e4f270422fa036331375
Praveen Kumar <praveenjillu17@gmail.com>
2025-02-19 14:44:23 +0530

    Add files via upload

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,8 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, DestroyRef, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TasksService } from '../tasks.service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval, Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-new-task',
@@ -17,5 +19,61 @@ export class NewTaskComponent {
 	onAddTask(title: string, description: string) {
 		this.tasksService.addTask({title,description});
 		this.formEl()?.nativeElement.reset();
+		// effect(() => {
+    	//   console.log(`Clicked button ${this.clickCount()} times.`);
+    	// });
 	}
+
+	clickCount = signal(0);
+	clickCount$ = toObservable(this.clickCount);
+	interval$ = interval(1000);
+	intervalSignal = toSignal(this.interval$, { initialValue: 0 });
+	// interval = signal(0);
+	// doubleInterval = computed(() => this.interval() * 2);
+	customInterval$ = new Observable((subscriber) => {
+		let timesExecuted = 0;
+		const interval = setInterval(() => {
+		// subscriber.error();
+		if (timesExecuted > 3) {
+			clearInterval(interval);
+			subscriber.complete();
+			return;
+		}
+		console.log('Emitting new value...');
+		subscriber.next({ message: 'New value' });
+		timesExecuted++;
+		}, 2000);
+	});
+	private destroyRef = inject(DestroyRef);
+
+	ngOnInit(): void {
+		// setInterval(() => {
+		//   this.interval.update(prevIntervalNumber => prevIntervalNumber + 1);
+		//   // update some signal
+		// }, 1000);
+		// const subscription = interval(1000).pipe(
+		//   map((val) => val * 2)
+		// ).subscribe({
+		//   next: (val) => console.log(val)
+		// });
+		// this.destroyRef.onDestroy(() => {
+		//   subscription.unsubscribe();
+		// });
+		this.customInterval$.subscribe({
+		next: (val) => console.log(val),
+		complete: () => console.log('COMPLETED!'),
+		error: (err) => console.log(err)
+		});
+		const subscription = this.clickCount$.subscribe({
+		next: (val) => console.log(`Clicked button ${this.clickCount()} times.`),
+		});
+		this.destroyRef.onDestroy(() => {
+		subscription.unsubscribe();
+		});
+	}
+
+	onClick() {
+		this.clickCount.update((prevCount) => prevCount + 1);
+	}
+
 }
